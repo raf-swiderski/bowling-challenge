f8b9bdbf85c95655f9014db1630430bb44ce7e0a
Raf Swiderski <rafswid@icloud.com>
2021-03-18 16:12:38 +0000

    js commit before change of approach

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -3,21 +3,51 @@ describe('Bowling', function() {
 
     beforeEach(function() {
         bowling = new Bowling();
+        bowling.start()
     });
     it('knows the current score', function() {
-        expect(bowling.score).toEqual(0);
+        expect(bowling.totalScore).toEqual(0);
     });
+
     it('knows when the game is in progress, and is not by default', function() {
+        bowling = new Bowling();
         expect(bowling.gameStatus).toBeFalsy();
     });
+
     it('has a method that starts a 1player game of bowling', function() {
-        bowling.start()
         expect(bowling.gameStatus).toBeTruthy();
     });
 
-    describe('start', function() {
-        it('restarts the loop for every frame')
+    it('bowling a strike adds 10 to the score', function() {
+        bowling.totalScore = 5;
+        bowling.roll(10)
+        expect(bowling.totalScore).toEqual(15);
     });
-    
+
+    it('bowling 2 consecutive strikes adds 30 to the score', function() {
+        bowling.totalScore = 5;
+        bowling.roll(10);
+        bowling.roll(10);
+        expect(bowling.totalScore).toEqual(35);
+    });
+
+    it('bowling less than 10 adds that number to the score', function() {
+        bowling.totalScore = 2;
+        bowling.roll(9);
+        expect(bowling.totalScore).toEqual(11);
+    });
+
+    it('rolling a strike then a 4 adds 18 to the score', function() {
+        bowling.roll(10);
+        bowling.roll(4);
+        expect(bowling.totalScore).toEqual(18);
+    });
+
+    it('', function() {
+        bowling.roll(10);
+        bowling.roll(4);
+        expect(bowling.totalScore).toEqual(18);
+    });
+
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -3,17 +3,40 @@
 
 class Bowling {
     constructor() {
-        this.score = 0;
+        this.totalScore = 0;
         this.gameStatus = false
+        this.bonusRounds = 0;
     }
 
     start() {
         this.gameStatus = true
-        for (let frame = 0; frame < 10; frame += 1) {
-            
-        }
     }
     
+    roll(roll) {
+        
+        if (roll === 10) {
+            
+            //strike
+            if (this.bonusRounds === 0) {
+                this.totalScore += 10
+                this.bonusRounds += 2
+            } else if (this.bonusRounds > 0) {
+                this.totalScore += 20
+                this.bonusRounds += 1
+            }
+
+        } else if (roll < 10) {
+            
+            if (this.bonusRounds === 0) {
+                this.totalScore += roll
+    
+            } else if (this.bonusRounds > 0) {
+                this.totalScore += roll * 2
+                this.bonusRounds -= 1
+            } 
+        }
+
+    }
     
 
 };
